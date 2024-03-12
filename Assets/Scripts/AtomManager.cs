defd3fe31080d5c551582714f414841387247e7f
BrodyMonsta <144285821+BrodyMonsta@users.noreply.github.com>
2024-03-12 00:39:23 -0400

    all Achievement added

---
+++ Assets/Scripts/AtomManager.cs
@@ -0,0 +1,339 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class AtomManager : MonoBehaviour
+{
+    public GameObject NaAtom;
+    public GameObject MgAtom;
+    public GameObject AlAtom;
+    public GameObject NAtom;
+    public GameObject OAtom;
+    public GameObject FAtom;
+    public GameObject Basket;
+
+    public GameObject OElectronParent;
+    public GameObject FElectronParent;
+    public int currentElement;
+    public int numberOfelectrons;
+    public int goodElectron;
+    /* public Transform[] NaelectronTransforms;
+     public Transform[] MgelectronTransforms;
+     public Transform[] AlelectronTransforms;
+    */
+    public GameObject[] NaElectrons;
+    public GameObject[] MgElectrons;
+    public GameObject[] AlElectrons;
+    public GameObject[] NElectrons;
+    public GameObject[] OElectrons;
+    public GameObject[] FElectrons;
+    public GameObject[] Titles;
+
+    //Current status of the table
+    public TMP_Text Tabletext;
+    //goal
+    public TMP_Text AtomText;
+
+    public void Awake()
+    {
+
+
+    }
+    // Set the current element in 1 , since is the first element that will spawn in the position
+    // of the second level.
+    private void Start()
+    {
+        /*foreach(GameObject electron in NaElectrons)
+         {
+             for(int i = 0; i < NaelectronTransforms.Length; i++)
+             {
+                 NaelectronTransforms[i] = electron.transform.;
+             }  
+
+         }
+        */
+        MgAtom.SetActive(false);
+        AlAtom.SetActive(false);
+        currentElement = 11;
+        numberOfelectrons = 11;
+        goodElectron = 1;
+        
+        Tabletext.text = "Na";
+
+        AtomText.text = "Na<sup>1+</sup>";
+
+    }
+
+
+    private void Update()
+    {
+        if (currentElement == 11)
+        {
+            if (numberOfelectrons == 0)
+            {
+                Tabletext.text = "Na";
+            }
+            else
+            {
+
+                Tabletext.text = "Na<sup>" + numberOfelectrons.ToString() + "+</sup>";
+            }
+
+        }
+        if (currentElement == 12)
+        {
+            if (numberOfelectrons == 0)
+            {
+                Tabletext.text = "Mg";
+            }
+            else
+            {
+                Tabletext.text = "Mg<sup>" + numberOfelectrons.ToString() + "+</sup>";
+            }
+        }
+        if (currentElement == 13)
+        {
+            if (numberOfelectrons == 0)
+            {
+                Tabletext.text = "Al";
+            }
+            else
+            {
+                Tabletext.text = "Al<sup>" + numberOfelectrons.ToString() + "+</sup>";
+            }
+
+        }
+        if (currentElement ==  14)
+        {
+            if (goodElectron == 0)
+            {
+                Tabletext.text = "N";
+            }
+            else
+            {
+                Tabletext.text = "N<sup>" + (goodElectron*-1).ToString() + "-</sup>";
+            }
+        }
+        if (currentElement == 15)
+        {
+            if (goodElectron == 0)
+            {
+                Tabletext.text = "O";
+            }
+            else
+            {
+                Tabletext.text = "O<sup>" + (goodElectron * -1).ToString() + "-</sup>";
+            }
+
+        }
+        if (currentElement == 16)
+        {
+            if (goodElectron == 0)
+            {
+                Tabletext.text = "F";
+            }
+            else
+            {
+                Tabletext.text = "F<sup>" + (goodElectron * -1).ToString() + "-</sup>";
+            }
+        }
+    }
+
+
+
+    //This function will be used to check if the electrons are getting out of the Atom 
+    // and spawning the next element once is finished.
+    private void checkingChanges()
+    {
+        if (currentElement == 11)
+        {
+            if (goodElectron == 1)
+            {
+                print("Its Working");
+                currentElement++;
+                NextElement();
+                numberOfelectrons = 12;
+                goodElectron = 2;
+                return;
+
+            }
+            else
+            {
+                print("Failed");
+            }
+        }
+
+        if (currentElement == 12)
+        {
+            if (goodElectron == 2)
+            {
+
+                print("Its Working");
+                currentElement++;
+                NextElement();
+                numberOfelectrons = 13;
+                goodElectron = 3;
+                return;
+            }
+            else
+            {
+                print("Failed");
+            }
+        }
+        if (currentElement == 13)
+        {
+            if (goodElectron == 3)
+            {
+
+                print("Its Working");
+                currentElement++;
+                NextElement();
+                numberOfelectrons = 8;
+                goodElectron = 0;
+            }
+            else
+            {
+                print("Failed");
+            }
+        }
+
+        if (currentElement == 14)
+        { 
+            if (goodElectron == -3)
+            {
+                print("Its Working");
+                currentElement++;
+                NextElement();
+                numberOfelectrons = 8;
+                goodElectron = 0;
+            }
+        }
+        if (currentElement == 15)
+        {
+            if (goodElectron == -2)
+            {
+                print("Its Working");
+                currentElement++;
+                NextElement();
+                numberOfelectrons = 7;
+                goodElectron = 0;
+            }
+        }
+        if (currentElement == 16)
+        {
+            if (goodElectron == -1)
+            {
+                print("Its Working");
+                currentElement++;
+                NextElement();
+                numberOfelectrons = 6;
+                goodElectron = 0;
+
+
+            }
+        }
+
+    }
+
+
+    // This function will check what elem ent is on play in that moment. 
+
+    private void NextElement()
+    {
+        if (currentElement == 12)
+        {
+            for (int i = 0; i < NaElectrons.Length; i++)
+            {
+                NaElectrons[i].SetActive(false);
+            }
+            NaAtom.SetActive(false);
+            MgAtom.SetActive(true);
+            Tabletext.text = "Mg";
+            AtomText.text = "Mg<sup>2+</sup>";
+
+        }
+        if (currentElement == 13)
+        {
+            for (int i = 0; i < MgElectrons.Length; i++)
+            {
+                MgElectrons[i].SetActive(false);
+            }
+            MgAtom.SetActive(false);
+            AlAtom.SetActive(true);
+            Tabletext.text = "Al";
+            AtomText.text = "Al<sup>3+</sup>";
+
+        }
+        if (currentElement == 14)
+        {
+            for (int i = 0; i < AlElectrons.Length; i++)
+            {
+                AlElectrons[i].SetActive(false);
+            }
+            AlAtom.SetActive(false);
+            NAtom.SetActive(true);
+            Tabletext.text = "N";
+            AtomText.text = "N<sup>3-</sup>";
+        }
+
+        if (currentElement == 15)
+        {
+            for (int i = 0; i < NElectrons.Length; i++)
+            {
+                NElectrons[i].SetActive(false);
+            }
+             NAtom.SetActive(false);
+             OAtom.SetActive(true);
+            OElectronParent.SetActive(true);
+            Tabletext.text="O";
+             AtomText.text= "O<sup>2-</sup>";
+
+         }
+         if (currentElement == 16)
+         {
+            for (int i = 0; i < OElectrons.Length; i++)
+            {
+                OElectrons[i].SetActive(false);
+            }
+            OAtom.SetActive(false);
+            FAtom.SetActive(true);
+           FElectronParent.SetActive(true);
+            Tabletext.text = "F";
+            AtomText.text = "F<sup>1-</sup>";
+
+         }
+
+        if(currentElement == 17)
+         {
+             FAtom.SetActive(false);
+             for (int i = 0; i<FElectrons.Length; i++)
+            {
+                FElectrons[i].SetActive(false);
+            }
+
+            SceneManager.LoadScene(3);
+         }
+     
+    }
+
+
+    // This is the function attached to the 3D UI Button that checks if there are changes
+    public void onPress()
+    {
+        Debug.Log("Is pressed");
+        checkingChanges();
+    }
+
+    /* public void ResetTransforms()
+     {
+         if(currentElement == 11)
+         {
+
+         }
+     }
+   */
+
+}
+
---
+++ Assets/Scripts/PT Manager.cs
@@ -0,0 +1,221 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PTManager : MonoBehaviour
+{
+    //Element Number, allows for identification of the inputted element.
+    public int elementNum = 0;
+    public int elementCount = 0;
+
+    //Key and renderer of the key object.
+    public GameObject key;
+    public GameObject keyLooks;
+    Renderer keyRend;
+
+    //Win and bonus condition ints.
+    public int winCondition = 10;
+    public int achievementCondition = 12;
+
+    //Booleans of the different elements.
+    public bool activeAluminum = false; //1
+    public bool activeCalcium = false; //2
+    public bool activeLithium = false; //3
+    public bool activeMagnesium = false; //4
+    public bool activeSodium = false; //5
+    public bool activePotassium = false; //6
+    public bool activeNitrogen = false; //7
+    public bool activeOxygen = false; //8
+    public bool activeFluorine = false; //9
+    public bool activePhosphorus = false; //10
+    public bool activeSulfur = false; //11
+    public bool activeChlorine = false; //12
+
+    //Panels of the different elements.
+    public GameObject panelAluminum;
+    public GameObject panelCalcium;
+    public GameObject panelLithium;
+    public GameObject panelMagnesium;
+    public GameObject panelSodium;
+    public GameObject panelPotassium;
+    public GameObject panelNitrogen;
+    public GameObject panelOxygen;
+    public GameObject panelFluorine;
+    public GameObject panelPhosphorus;
+    public GameObject panelSulfur;
+    public GameObject panelChlorine;
+
+    //Gets the renderer of the key at the start of the game to be able to change its color and sets it to false.
+    private void Start()
+    {
+        //Deactivate key and get the renderer.
+        key.SetActive(false);
+        keyRend = keyLooks.GetComponent<Renderer>();
+
+        //Deactivate the panels.
+        panelAluminum.SetActive(false);
+        panelCalcium.SetActive(false);
+        panelLithium.SetActive(false);
+        panelMagnesium.SetActive(false);
+        panelSodium.SetActive(false);
+        panelPotassium.SetActive(false);
+        panelNitrogen.SetActive(false);
+        panelOxygen.SetActive(false);
+        panelFluorine.SetActive(false);
+        panelPhosphorus.SetActive(false);
+        panelSulfur.SetActive(false);
+        panelChlorine.SetActive(false);
+    }
+
+    /*************************************************************************************************
+     * Gets the Element Number from the PT Interactions script, puts it inside of a switch statement *
+     * and uses it to check witch of the elements have been put correctly inside the periodic table, *
+     * after that increases the count of elements gotten right and checks if the win condition has   *
+     * been met, also the if statement and boolean unables this to be repeated.                      *
+     *************************************************************************************************/
+    public void countNActivate(int i)
+    {
+        elementNum = i;
+
+        switch (elementNum)
+        {
+            case 0:
+                Debug.Log("Didn't assign anything.");
+                break;
+
+            case 1:
+                if (activeAluminum == false)
+                {
+                    activeAluminum = true;
+                    panelAluminum.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 2:
+                if (activeCalcium == false)
+                {
+                    activeCalcium = true;
+                    panelCalcium.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 3:
+                if (activeLithium == false)
+                {
+                    activeLithium = true;
+                    panelLithium.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 4:
+                if (activeMagnesium == false)
+                {
+                    activeMagnesium = true;
+                    panelMagnesium.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 5:
+                if (activeSodium == false)
+                {
+                    activeSodium = true;
+                    panelSodium.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 6:
+                if (activePotassium == false)
+                {
+                    activePotassium = true;
+                    panelPotassium.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 7:
+                if (activeNitrogen == false)
+                {
+                    activeNitrogen = true;
+                    panelNitrogen.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 8:
+                if (activeOxygen == false)
+                {
+                    activeOxygen = true;
+                    panelOxygen.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 9:
+                if (activeFluorine == false)
+                {
+                    activeFluorine = true;
+                    panelFluorine.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 10:
+                if (activePhosphorus == false)
+                {
+                    activePhosphorus = true;
+                    panelPhosphorus.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 11:
+                if (activeSulfur == false)
+                {
+                    activeSulfur = true;
+                    panelSulfur.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+
+            case 12:
+                if (activeChlorine == false)
+                {
+                    activeChlorine = true;
+                    panelChlorine.SetActive(true);
+                    elementCount++;
+                    keyCheck();
+                }
+                break;
+        }
+    }
+
+    //Checks for the win conditions.
+    void keyCheck()
+    {
+        if (elementCount == winCondition) //Win Condition.
+        {
+            key.SetActive(true);
+            keyRend.material.color = Color.yellow;
+        }
+        else if (elementCount == achievementCondition) //Achievement.
+        {
+            keyRend.material.color = Color.red;
+        }
+    }
+}
