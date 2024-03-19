f37e162e08b26da3e59feeee1fdd9b1b3da92ec3
EliasM9887 <eliasm9887gmail.com>
2024-03-18 23:53:46 -0400

    Resize, colliders, and Player

---
+++ Assets/Scripts/ExperimentBool.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperimentBool : MonoBehaviour
+{
+    public bool hasBeenExperimentedOn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
